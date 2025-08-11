e41c1c06235847e9f2eef5916bf52c77f7d164b7
Alam <alam@Alams-MacBook-Pro.local>
2025-08-11 19:24:48 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM node:20.17.0-alpine AS build
+
+# Set the working directory
+WORKDIR /usr/local/app
+
+# RUN npm cache clean --force
+
+# Add the source code to app
+COPY ./ /usr/local/app/
+RUN ls  /usr/local/app
+# Install all the dependencies
+RUN npm install
+
+# Generate the build of the application
+RUN npm run build
+
+RUN ls  /usr/local/app
+# Stage 2: Serve app with nginx server
+RUN ls  /usr/local/app/dist/keycloak-angular-example/browser
+# Use official nginx image as the base image
+FROM nginx:latest
+# Copy the build output to replace the default nginx contents.
+RUN echo $(ls -1 /usr/local/app)
+COPY --from=build /usr/local/app/dist/keycloak-angular-example/browser /usr/share/nginx/html
+COPY ./nginx.conf  /etc/nginx/nginx.conf
+RUN ls  /usr/share/nginx/html
+EXPOSE 8080:8080
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/app/app.config.ts
@@ -0,0 +1,42 @@
+import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { KeycloakAngularModule, KeycloakBearerInterceptor, KeycloakService } from 'keycloak-angular';
+
+import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'https://wealth-keycloak.apps-crc.testing',
+        realm: 'master',
+        clientId: 'angular-client'
+      },
+      initOptions: {
+        onLoad: 'check-sso', // 'login-required' , 'check-sso'
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      },
+      enableBearerInterceptor: true
+    });
+}
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    },
+    KeycloakService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: KeycloakBearerInterceptor,
+      multi: true
+    }
+  ]
+};
